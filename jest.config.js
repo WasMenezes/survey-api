1a44498e70ec15f9206953d02b60287545319b41
wasmenezes <was288519@gmail.com>
2021-01-21 05:48:48 -0300

    refactor: move mongodb methods to a helper

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
