89b8036eac614fca31909cce68e367ded34f00c2
wasmenezes <was288519@gmail.com>
2021-02-02 05:43:58 -0300

    refactor: move EmailValidatoin to the ValidationComposite

--- src/presentation/controllers/signup/singup.spec.ts
+++ src/presentation/controllers/signup/singup.spec.ts
@@ -1,18 +1,9 @@
 import { SignUpController } from './singup'
-import { AddAccount, AddAccountModel, EmailValidator, HttpRequest } from './singup-protocols'
-import { MissingParamError, InvalidParamError, ServerError } from '../../errors'
+import { AddAccount, AddAccountModel, HttpRequest } from './singup-protocols'
+import { MissingParamError, ServerError } from '../../errors'
 import { AccountModel } from '../../../domain/models/account'
 import { ok, serverError, badRequest } from '../../helpers/http-helper'
 import { Validation } from '../../helpers/validators/validation'
-
-const makeEmailValidator = (): EmailValidator => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid (email: string): boolean {
-      return true
-    }
-  }
-  return new EmailValidatorStub()
-}
 
 const makeAddAccount = (): AddAccount => {
   class AddAccountStub implements AddAccount {
@@ -51,50 +42,21 @@ const makeFakeAccount = (): AccountModel => ({
 
 interface SutTypes {
   sut: SignUpController
-  emailValidatorStub: EmailValidator
   addAccountStub: AddAccount
   validationStub: Validation
 }
 
 const makeSut = (): SutTypes => {
-  const emailValidatorStub = makeEmailValidator()
   const addAccountStub = makeAddAccount()
   const validationStub = makeValidation()
-  const sut = new SignUpController(emailValidatorStub, addAccountStub, validationStub)
+  const sut = new SignUpController(addAccountStub, validationStub)
   return {
-    emailValidatorStub,
     sut,
     addAccountStub,
     validationStub
   }
 }
 describe('SignUp Controller ', () => {
-  test('should return 400 if an invalid email is provided', async () => {
-    const { sut, emailValidatorStub } = makeSut()
-    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
-    const httpResponse = await sut.handle(makeFakeRequest())
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
-  })
-
-  test('should call EmailValidator with correct email', async () => {
-    const { sut, emailValidatorStub } = makeSut()
-    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
-    const httpRequest = makeFakeRequest()
-    await sut.handle(httpRequest)
-    expect(isValidSpy).toHaveBeenCalledWith(httpRequest.body.email)
-  })
-
-  test('should return 500 if EmailValidator throws', async () => {
-    const { sut, emailValidatorStub } = makeSut()
-    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
-      throw new Error()
-    })
-    const httpRequest = makeFakeRequest()
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(serverError(new ServerError()))
-  })
-
   test('should call AddAccount with correct values', async () => {
     const { sut, addAccountStub } = makeSut()
     const addSpy = jest.spyOn(addAccountStub, 'add')
--- src/presentation/controllers/signup/singup.ts
+++ src/presentation/controllers/signup/singup.ts
@@ -1,14 +1,11 @@
-import { InvalidParamError } from '../../errors'
-import { HttpResponse, HttpRequest, Controller, AddAccount, EmailValidator, Validation } from './singup-protocols'
+import { HttpResponse, HttpRequest, Controller, AddAccount, Validation } from './singup-protocols'
 import { badRequest, serverError, ok } from '../../helpers/http-helper'
 
 export class SignUpController implements Controller {
-  private readonly emailValidator: EmailValidator
   private readonly addAcount: AddAccount
   private readonly validation: Validation
 
-  constructor (emailValidator: EmailValidator, addAccount: AddAccount, validation: Validation) {
-    this.emailValidator = emailValidator
+  constructor (addAccount: AddAccount, validation: Validation) {
     this.addAcount = addAccount
     this.validation = validation
   }
@@ -20,18 +17,11 @@ export class SignUpController implements Controller {
         return badRequest(error)
       }
       const { name, email, password } = httpRequest.body
-
-      const isValid = this.emailValidator.isValid(email)
-      if (!isValid) {
-        return badRequest(new InvalidParamError('email'))
-      }
-
       const account = await this.addAcount.add({
         name,
         email,
         password
       })
-
       return ok(account)
     } catch (error) {
       console.error(error)
