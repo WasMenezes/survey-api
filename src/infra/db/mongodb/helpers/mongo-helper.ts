1c9fa5d6353c33b01837aaef84268191a352df7f
wmenezes <wmenezes@CPD11>
2021-04-08 13:52:26 -0300

    feat: ensure SurveyResultMongoRepository insert new surveyResult if is new

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,5 +1,4 @@
 import { Collection, MongoClient } from 'mongodb'
-import { AccountModel } from '@/domain/models/account'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
@@ -25,7 +24,7 @@ export const MongoHelper = {
     return this.client.db().collection(name)
   },
 
-  map: (collection: any): AccountModel => {
+  map: (collection: any): any => {
     const { _id, ...collectionWithoutId } = collection
     return Object.assign({}, collectionWithoutId, { id: _id })
   }
